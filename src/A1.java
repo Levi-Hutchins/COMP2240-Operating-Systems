81cd147545deba5c284d6a03a53948061b841522
Levi Hutchins <levihutchins0@gmail.com>
2023-08-05 22:00:44 +1000

    commit

---
+++ src/A1.java
@@ -0,0 +1,14 @@
+import java.io.ObjectInputFilter.Config;
+
+public class A1 {
+    public static void main(String[] args) {
+        if (args.length != 1){
+            throw new IllegalArgumentException("Please Enter Data File");
+        }
+        
+    }
+    public void exe(String[] ags){
+
+    }
+    
+}
