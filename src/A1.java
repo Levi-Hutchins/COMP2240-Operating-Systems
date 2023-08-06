1848454d7cdf87209d86a649557806979b8875a6
Levi Hutchins <levihutchins0@gmail.com>
2023-08-06 18:10:16 +1000

    datafile reading

--- src/A1.java
+++ src/A1.java
@@ -18,18 +18,45 @@ public class A1 {
         return;
     }
     public void readFileData(String file){
+        int DISP = 0;
+
+        String pid = "";
+        int arr_time = 0;
+        int srv_time = 0;
+        int priority = 0;
+    
+
         try{ 
             Scanner reader = new Scanner( new File(file));
             while(reader.hasNextLine()){
                 String line = reader.nextLine();
-                if(line.equals("END")){
-                    System.out.println("Here");
+                if(line.contains("DISP")){
+                    System.out.println(line);
+                    DISP = Integer.parseInt(line.substring(6));
+                }
+                if(line.contains("PID")){
+                    pid = line.substring(5);
+                }
+                if(line.contains("ArrTime")){
+                    arr_time = Integer.parseInt(line.substring(9));
+                }
+                if(line.contains("SrvTime")){
+                    srv_time = Integer.parseInt(line.substring(9));
+                }
+                if(line.contains("Priority")){
+                    priority = Integer.parseInt(line.substring(10));
+                }
+                if(line.contains("END")){
+                    System.out.println(pid);
+                    System.out.println(arr_time);
+                    System.out.println(srv_time);
+                    System.out.println(priority);
+
                 }
             }
 
         }
         catch(FileNotFoundException e){ System.out.println("An Error Occured");}
-
 
     }
     
